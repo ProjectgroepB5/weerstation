0907d25bc6ddf79e8065ae72639de2b0ac3f4357
Dofensmirtsz <dofensmirtsz@gmail.com>
2014-10-07 14:02:42 +0200

    Update and rename BuitenLuchtVochtigheid.java to OutsideHum.java
    GEMAAKT DOOR TIM BITCHESSSS

--- OutsideHum.java
+++ OutsideHum.java
@@ -4,42 +4,42 @@ import java.util.ArrayList;
 /**
  * Write a description of class OpdrachtDinges here.
  * 
- * @author (your name) 
+ * @author (Tim van Lieshout) 
  * @version (a version number or a date)
  */
-public class BuitenLuchtvochtigheid
+public class OutsideHum
 {
-    Weerstation weerstation;
-    Measurement meting;
+    Weerstation weerstation; //connectie maken met Weerstation klasse
+    Measurement meting; //connectie maken met Measurement klasse
     ArrayList<Measurement> laatste24uur; //ArrayList om de luchtvochtigheid op te slaan
     
-    public BuitenLuchtvochtigheid()
+    public OutsideHum()
     {
             weerstation = new Weerstation();  //maakt een nieuw weerstation aan
             meting = weerstation.getMostRecentMeasurement(); //pakken recenste gegevens MOET VERWIJDERD WORDEN
     }
     
-    public double getBuitenLuchtvochtigheid()
+    public double buitenLuchtvochtigheid()
     {
         IO.init();
-        meting.getOutsideHum();
+        meting.getOutsideHum(); //waardes van buitenluchtvochtigheid ophalen uit de meting klasse
         return meting.getOutsideHum();
     }
     
     public void getMaximale()
     {
         IO.init();
-        laatste24uur = weerstation.getAllMeasurementsLast24h();
+        laatste24uur = weerstation.getAllMeasurementsLast24h(); //geef inhoud aan de (nu nog lege) ArrayList laatste24uur
         
         short maximale = 0;
-        for(int i=0; i < laatste24uur.size();i++)
+        for(int i=0; i < laatste24uur.size();i++) //zolang i kleiner is dan de grootte van de ArrayList laatste24uur voert het de if-statement uit
         {
-            if(laatste24uur.get(i).getRawOutsideHum() > maximale)
+            if(laatste24uur.get(i).getRawOutsideHum() > maximale) //plek (i) in de ArrayList laatste24uur vraag je de waarde RawOutsideHum van op. Als die groter is dan huidige maximale, vervang je maximale door dat getal.
             {
                 maximale = laatste24uur.get(i).getRawOutsideHum();
             }
         }
-        System.out.println(maximale);
+        System.out.println(maximale); //laat de waarde van maximale zien.
     }
     
     public void getMinimale()
@@ -60,7 +60,16 @@ public class BuitenLuchtvochtigheid
     
     public void getAverage()
     {
+        IO.init();
+        laatste24uur = weerstation.getAllMeasurementsLast24h();
         
+        int average = 0;
+        for(int i=0; i < laatste24uur.size(); i ++)
+        {
+            average += laatste24uur.get(i).getRawOutsideHum();
+        }
+        average /= laatste24uur.size();
+        System.out.println(average);
     }
     
     public void matrixScherm()
