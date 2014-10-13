7a2154f8463d01db6ddf0f2afe3a865c72b36f77
jancoow <jancokock@gmail.com>
2014-10-13 11:54:08 +0200

    Added sun class

---
+++ Sun.java
@@ -0,0 +1,49 @@
+package weerstation;
+
+import java.util.ArrayList;
+
+public class Sun {
+	
+	//fields
+	private String sunSet;
+	private String sunRise;
+	
+	//constructor
+	public Sun(Measurement measurement1){
+		updateRecent(measurement1);
+	}
+	
+	//Getters and setters
+	
+	public String getSunSet() {
+		return sunSet;
+	}
+
+	public void setSunSet(String sunSet) {
+		this.sunSet = sunSet;
+	}
+
+	public String getSunRise() {
+		return sunRise;
+	}
+
+	public void setSunRise(String sunRise) {
+		this.sunRise = sunRise;
+	}
+	
+	public void updateRecent(Measurement measurement1){
+		setSunSet(measurement1.getSunset());
+		setSunRise(measurement1.getSunrise());
+	}
+	
+	public void update24Hour(ArrayList<Measurement> measurement2){
+	}
+	
+	public void display(){		
+		GUIboard.clearLeft();
+		GUIboard.clearRight();
+		GUIboard.clearTop();
+		GUIboard.writePageToMatrix("Sunset: " + sunSet, "Sunrise: " + sunRise, "");
+	}
+	
+}
