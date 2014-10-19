cbd4ef907772e18ec7facc1f0977eea8132e9ae1
jancoow <jancokock@gmail.com>
2014-10-19 14:13:56 +0200

    Will now calculate the mode, median and deviation

--- Grootheid.java
+++ Grootheid.java
@@ -1,3 +1,4 @@
+package weerstation1;
 import java.util.ArrayList;
 
 public class Grootheid
@@ -6,6 +7,9 @@ public class Grootheid
     private double avg;
     private double max;
     private double min;
+    private double mode;
+    private double median;
+    private double deviation;
     private double current;
 
     //constructor
@@ -49,11 +53,38 @@ public class Grootheid
         this.avg = avg;
     }
     
-    //Methods
+    public double getMode() {
+		return mode;
+	}
+
+	public void setMode(double mode) {
+		this.mode = mode;
+	}
+
+	public double getMedian() {
+		return median;
+	}
+
+	public void setMedian(double median) {
+		this.median = median;
+	}
+
+	public double getDeviation() {
+		return deviation;
+	}
+
+	public void setDeviation(double deviation) {
+		this.deviation = deviation;
+	}
+
+	//Methods
     public void calculateMaxMinAvg(ArrayList<Double> array){
         setMax( StatisticsCalculator.max(array) );
         setMin( StatisticsCalculator.min(array) );
         setAvg( StatisticsCalculator.avg(array) );
+        setMedian( StatisticsCalculator.median(array) );
+        setMode( StatisticsCalculator.modus(array) );
+        setDeviation( StatisticsCalculator.afwijking(array) );
     }
 
     public void updateRecent(Measurement measurement1){
